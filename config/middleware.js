7467f6974384aa5b2e67b193a42588a1519f517f
sxmxc <steven.mcneely@live.com>
2021-08-26 03:15:17 +0000

    env.example

---
+++ config/middleware.js
@@ -0,0 +1,9 @@
+module.exports = {
+    //...
+    settings: {
+      cors: {
+        origin: ['*'], //allow all
+      },
+    },
+  };
+  
--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
-  url: env('PUBLIC_URL', 'http://api.voidmoose.net'),
-  proxy: env.bool('IS_PROXIED', true),
+  url: env('PUBLIC_URL', 'http://api.voidmoose.lan:1337'),
+  proxy: env.bool('IS_PROXIED', false),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', 'eca3a10daff66e6e605b7ae5262017cb'),
